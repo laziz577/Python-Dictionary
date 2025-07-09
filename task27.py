33789169c8dbb9b3b3b4e9895c04584ff7a2c6cf
laziz577 <temirovabdulaziz042@gmail.com>
2025-07-09 11:53:58 +0500

    bajarildi

---
+++ task27.py
@@ -0,0 +1,60 @@
+from time import sleep
+from termcolor import colored
+
+
+def print_menu() -> None:
+    print("========== Menu ==========")
+    print("1: Kontakt qo'shish (ism → telefon)")
+    print("2: Barcha kontaktlarni chiqarish")
+    print("3: Ism bo'yicha telefon qidirish")
+    print("4: Dasturdan chiqish")
+
+def add_contact(phonebook: dict[str, str]) -> None:
+    print("Contact malumotlarini kiriting")
+    name = input("Name: ").strip().title()
+    phone = input("Phone: ")
+
+    if name in phonebook.keys() or not phone.isdigit():
+        print(colored("xatolik yuz berdi.","red"))
+        return
+    phonebook[name] = phone
+    print(colored("qoshildi", "green"))
+
+def show_contact(phonebook: dict[str, str]) -> None:
+    if not phonebook:
+        print(colored("contact mavjud emas", "red"))
+        return
+    
+    print("Barcha contactlar")
+    for number, contact in enumerate(phonebook, start=1):
+        print(f"{number}. {contact} → {phonebook[contact]}")
+def search_contact(phonebook: dict[str, str]) -> None:
+    search = input("Qidirayotgan contact:").strip().title()
+    if search in phonebook.keys():
+        print(f"{search} -> {phonebook[search]}")
+    else:
+        print(colored("topilmadi.","red"))
+
+def phonebook_menu(phonebook: dict[str, str]) -> None:
+    while True:
+        print_menu()
+
+        choice = input(colored("Menu Tanlang: ", "blue"))
+        if choice == '1':
+            add_contact(phonebook)
+        elif choice == '2':
+            show_contact(phonebook)
+        elif choice == '3':
+            search_contact(phonebook)
+        elif choice == '4':
+            break
+        else:
+            print(colored("Bunday menu mavjud emas.", "red"))
+
+        print(colored("\n------------------------------\n", "blue"))
+        
+        sleep(2)
+
+
+phonebook = dict()
+phonebook_menu(phonebook)
